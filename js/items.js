0f6a8cced0e149138cc3c1a661eb61870c2443cf
sumoondev <sumoon.dev@gmail.com>
2026-01-31 22:55:05 +0545

    feat: createItems()

---
+++ js/items.js
@@ -0,0 +1,13 @@
+import { createSingleItem } from "./single-item.js";
+
+export function createItems(itemsArray) {
+    const container = document.createElement("div");
+    container.className = "items";
+
+    itemsArray.forEach((item) => {
+        const itemElement = createSingleItem(item);
+        container.appendChild(itemElement);
+    });
+
+  return container;
+}
--- js/single-item.js
+++ js/single-item.js
@@ -1,12 +1,12 @@
 export function createSingleItem(item) {
-  const div = document.createElement("div");
-  div.className = "single-item";
+    const div = document.createElement("div");
+    div.className = "single-item";
 
-  div.innerHTML = `
-        <input type="checkbox" ${item.completed ? "checked" : ""}>
-        <p style="text-decoration: ${item.completed ? "line-through" : "none"}">${item.name}</p>
-        <button class="btn icon-btn edit-btn" type="button"><i class="fa-regular fa-pen-to-square"></i></button>
-        <button class="btn icon-btn remove-btn" type="button"><i class="fa-regular fa-square-plus"></i></button>
-  `;
-  return div;
+    div.innerHTML = `
+            <input type="checkbox" ${item.completed ? "checked" : ""}>
+            <p style="text-decoration: ${item.completed ? "line-through" : "none"}">${item.name}</p>
+            <button class="btn icon-btn edit-btn" type="button"><i class="fa-regular fa-pen-to-square"></i></button>
+            <button class="btn icon-btn remove-btn" type="button"><i class="fa-regular fa-square-plus"></i></button>
+    `;
+    return div;
 }
